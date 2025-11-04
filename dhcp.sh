bcb4538f8eab686b2a4604ba3a70a102b32d83c6
DarenVejarano <darenandrevejarano@paucasesnovescifp.cat>
2025-11-04 18:28:33 +0100

    DHCP KEA

--- dhcp.sh
+++ dhcp.sh
@@ -7,18 +7,19 @@
 #
 # declare STRING variable
 STRING="Scripts examples"
-f_dchpd_conf="https://raw.githubusercontent.com/AlexGomila/dhcp/refs/heads/main/kea-dhcp4.conf"
+f_dchpd_conf="https://raw.githubusercontent.com/DarenVejarano/Kea-dhcp-SCRIPT/refs/heads/main/KEA.conf"
 #print variable on a screen
 echo $STRING
 
 #Install DHCP
-apt-get install kea
+apt-get install -y kea-dhcp4-server
+
 rm  /etc/kea/kea-dhcp4.conf
 # download githup configuration file
 wget $f_dchpd_conf
 
 # copy configuration file to etc directory
-cp kea-dhcp4.conf /etc/kea
+cp KEA.conf /etc/kea
 
 systemctl restart kea-dhcp4-server
 systemctl status kea-dhcp4-server
