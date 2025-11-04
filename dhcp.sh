a65b212dbc60af65c92d922786d7ec33338aa611
DarenVejarano <darenandrevejarano@paucasesnovescifp.cat>
2025-11-04 18:08:33 +0100

    DHCP KEA
    Updated the DHCP server installation and configuration process to use Kea DHCP instead of ISC DHCP. Removed old configuration steps and added new commands for Kea setup.

--- dhcp.sh
+++ dhcp.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
-set -e
+# Purpose: Config ubuntu DHCP server
+# https://ubuntu.com/server/docs/network-dhcp
+# Author: Jose Maria Madronal GPL v2.0+
+# ------------------------------------------
+#
+#
+# declare STRING variable
+STRING="Scripts examples"
+f_dchpd_conf="https://raw.githubusercontent.com/AlexGomila/dhcp/refs/heads/main/kea-dhcp4.conf"
+#print variable on a screen
+echo $STRING
 
-echo "[INFO] Instalando servidor DHCP..."
-apt update && apt install -y isc-dhcp-server
+#Install DHCP
+apt-get install kea
+rm  /etc/kea/kea-dhcp4.conf
+# download githup configuration file
+wget $f_dchpd_conf
 
-IFACE="eth0"
-echo "[INFO] Configurando interfaz DHCP en $IFACE..."
-sed -i "s/^INTERFACESv4=.*/INTERFACESv4=\"$IFACE\"/" /etc/default/isc-dhcp-server
+# copy configuration file to etc directory
+cp kea-dhcp4.conf /etc/kea
 
-echo "[INFO] Creando configuración DHCP..."
-cat > /etc/dhcp/dhcpd.conf <<EOF
-option domain-name "local";
-option domain-name-servers 8.8.8.8, 1.1.1.1;
-default-lease-time 600;
-max-lease-time 7200;
-authoritative;
-
-subnet 10.18.41.0 netmask 255.255.255.224 {
-  range 10.18.41.10 10.18.41.25;
-  option routers 10.18.41.1;
-  option broadcast-address 10.18.41.31;
-}
-EOF
-
-echo "[INFO] Reiniciando servicio DHCP..."
-systemctl restart isc-dhcp-server
-systemctl enable isc-dhcp-server
-
-echo "[OK] Servidor DHCP activo en $(hostname -I | awk '{print $1}')"
+systemctl restart kea-dhcp4-server
+systemctl status kea-dhcp4-server
